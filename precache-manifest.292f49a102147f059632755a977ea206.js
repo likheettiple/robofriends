9faa296696c4c45334ce3da17a095caf37adad75
likheettiple <7likheet@gmail.com>
2020-06-05 22:58:09 +0530

    Updates

---
+++ precache-manifest.292f49a102147f059632755a977ea206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1be5242aa96fd1e999fc353401e4e9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c83e07f37480374bdc52",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "978c7ddecb93c56c29fa",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c83e07f37480374bdc52",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978c7ddecb93c56c29fa",
+    "url": "/robofriends/static/js/main.fea0db63.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
